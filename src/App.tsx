14f211aa440720e820cbbe80a91890be40c8de9b
Janaina Francine Ribeiro-Locatelli <janaina_f_ribeiro-locatelli@whirlpool.com>
2022-03-18 12:27:33 -0400

    initial development

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, TextField } from '@mui/material';
 
-function App() {
+const App = () => {
+  const [myItems, setMyItems] = useState(["Lucas","Jana"]);
+  const [myItems2, setMyItems2] = useState(["Leah"]);
+  const [counter,setCounter] = useState(0); 
+
+  console.log(encodeURIComponent('saint joseph,mi') );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant='contained' onClick={() => setCounter(counter+1)}>Count Up</Button>
+      <p>{counter}</p>
+      <TextAdder onAdd={(text) => setMyItems([...myItems,text])}></TextAdder>
+      {myItems.map((item,index) => <p key={index}>{item}</p>)}
+      <TextAdder color='cyan' onAdd={(text) => setMyItems2([...myItems2,text])}></TextAdder>
+      {myItems2.map((item,index) => <p key={index}>{item}</p>)}
     </div>
   );
 }
+
+interface TextAdderProps {
+  onAdd: (text: string) => void,
+  color?: string
+}
+
+const TextAdder = (props:TextAdderProps) => {
+const [text,setText] = useState(""); 
+return (      
+<div>
+  <TextField onChange={(e) => setText(e.target.value)}></TextField>
+  <Button variant='contained' style={{margin:"10px", color:`${props.color}`}} onClick={() => { 
+    props.onAdd(text);
+  }
+  }>Add Text</Button>
+</div>);
+}
+
+interface InputWithButtonProps {
+  onCommit: (text: string) => void,
+  color?: string
+}
+
+const InputWithButton : React.FC<InputWithButtonProps> = (props) => {
+  const [text,setText] = useState(""); 
+  return (      
+  <div>
+    <TextField onChange={(e) => setText(e.target.value)}></TextField>
+    <Button variant='contained' style={{margin:"10px", color:`${props.color}`}} onClick={() => { 
+      props.onCommit(text);
+    }
+    }>{props.children}</Button>
+  </div>);
+  }
 
 export default App;
